261da785c35c5629c2e55005e6632e799ae6ef69
Francesco Romani <fromani@redhat.com>
2019-05-07 15:44:40 +0200

    travis: fix automation
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/container/docker-push.sh
+++ hack/container/docker-push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-TAG="${1:-devel}"
+set -e
 
-docker build -t quay.io/fromani/kubevirt-template-validator:$TAG . && \
-docker push quay.io/fromani/kubevirt-template-validator:$TAG
+VERSION="${1:-devel}"
+
+echo "$QUAY_BOT_PASS" | docker login -u="$QUAY_BOT_USER" --password-stdin quay.io
+docker build -t quay.io/fromani/kubevirt-template-validator:$VERSION .
+docker push quay.io/fromani/kubevirt-template-validator:$VERSION

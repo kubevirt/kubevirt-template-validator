dfb7c4460ba9ca6ca01bc27fe630c0f8a6f3fc48
Francesco Romani <fromani@redhat.com>
2019-05-08 17:26:19 +0200

    travis: run tests on PR
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/container/docker-push.sh
+++ hack/container/docker-push.sh
@@ -4,6 +4,11 @@ set -e
 
 VERSION="${1:-devel}"
 
+if [ -z "$QUAY_BOT_PASS" ] || [ -z "$QUAY_BOT_USER" ]; then
+	echo "missing QUAY_BOT_{USER,PASS} env vars"
+	exit 1
+fi
+
 echo "$QUAY_BOT_PASS" | docker login -u="$QUAY_BOT_USER" --password-stdin quay.io
 docker build -t quay.io/fromani/kubevirt-template-validator:$VERSION .
 docker push quay.io/fromani/kubevirt-template-validator:$VERSION

039887c3420c7751f4a8b87501af9c132b5b522d
Francesco Romani <fromani@redhat.com>
2019-05-27 19:12:57 +0200

    functests: add sleep after template submission
    reduce false negative when the test host is too slow
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- functests/07-test-with-template-with-incorrect-rules-satisfied.sh
+++ functests/07-test-with-template-with-incorrect-rules-satisfied.sh
@@ -2,6 +2,7 @@
 {
 RET=0
 $KUBECTL create -n default -f manifests/template-with-rules-incorrect.yaml  || exit 2
+sleep 1s
 if $KUBECTL create -f manifests/07-vm-from-template-with-incorrect-rules-satisfied.yaml ;  then
 	RET=1
 	$KUBECTL delete vm vm-test-07
--- functests/08-test-with-template-with-incorrect-rules-unfulfilled.sh
+++ functests/08-test-with-template-with-incorrect-rules-unfulfilled.sh
@@ -2,6 +2,7 @@
 {
 RET=0
 $KUBECTL create -n default -f manifests/template-with-rules-incorrect.yaml  || exit 2
+sleep 1s
 if $KUBECTL create -f manifests/08-vm-from-template-with-incorrect-rules-unfulfilled.yaml ; then
 	RET=1
 	$KUBECTL delete vm vm-test-08

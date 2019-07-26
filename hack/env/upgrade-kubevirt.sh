84c5aa3d1c579f819096247d08e691426e28cb1a
Francesco Romani <fromani@redhat.com>
2019-07-26 14:53:41 +0200

    upgrade-kubevirt: workaround for k6 "issue" 2533
    Is not really an issue, but we need to upgrade the virt-operator before
    to upgrade kubevirt itself.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/env/upgrade-kubevirt.sh
+++ hack/env/upgrade-kubevirt.sh
@@ -6,6 +6,11 @@ BASEPATH=$( dirname $SELF )
 
 # we can't guess.
 [ -z "$1" ] && exit 1
+RELEASE="${1}"
 
-oc patch kv kubevirt -n kubevirt --type=json -p "[{ \"op\": \"add\", \"path\": \"/spec/imageTag\", \"value\": \"$1\" }]"
+# avoids https://github.com/kubevirt/kubevirt/issues/2533
+oc apply -f https://github.com/kubevirt/kubevirt/releases/download/${RELEASE}/kubevirt-operator.yaml
+# TODO: smarter wait
+sleep 42s
+oc patch kv kubevirt -n kubevirt --type=json -p "[{ \"op\": \"add\", \"path\": \"/spec/imageTag\", \"value\": \"${RELEASE}\" }]"
 

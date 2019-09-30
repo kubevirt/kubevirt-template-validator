e52dc90d0dd9fb3c609eba0bcc845af68eed6ab7
Francesco Romani <fromani@redhat.com>
2019-09-30 10:19:48 +0200

    travis: fetch third party data earlier
    To test the upgrade path, we need the kubevirt release info
    from github.
    In the last few weeks, it happened few times that the fetch
    operation failed late in the build (we used to do the fetch just in
    time), wasting a lot of time.
    
    We move the fetch of the github-dependent info as early as possible
    in the build, and we cache locally. This way, if the fetch fails,
    we know early and we waste as little as possible.
    
    Having the versions info cached allows us also to simplify
    some upgrade steps.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/env/upgrade-and-test.sh
+++ hack/env/upgrade-and-test.sh
@@ -4,16 +4,36 @@ set -ex
 SELF=$( realpath $0 )
 BASEPATH=$( dirname $SELF )
 
-TARGET_VERSION="${1}"
-CURRENT_VERSION="${2}"
-if [ "$1" == "latest" ]; then
-	TARGET_VERSION=$( curl --silent -k "https://api.github.com/repos/kubevirt/kubevirt/releases" |  ${BASEPATH}/latest-versions.py )
-fi
+STEP="${1}"
+
+CURRENT_VERSION="unknown"
+TARGET_VERSION="unknown"
+
+PREVIOUS_VERSION=$( awk -F= '/^previous=/ { print $2 }' versionsrc )
+BUILTIN_VERSION=$( awk -F= '/^builtin=/ { print $2 }' versionsrc )
+LAST_VERSION=$( awk -F= '/^last=/ { print $2 }' versionsrc )
+
+case $STEP in
+	old-to-builtin)
+		CURRENT_VERSION="$PREVIOUS_VERSION"
+		TARGET_VERSION="$BUILTIN_VERSION"
+		;;
+	builtin-to-new)
+		CURRENT_VERSION="$BUILTIN_VERSION"
+		TARGET_VERSION="$LAST_VERSION"
+		;;
+	*)
+		exit 1
+		;;
+esac
+
 
 if [ "${TARGET_VERSION}" == "${CURRENT_VERSION}" ]; then
 	echo "nothing to do: target == current"
 	exit 0
 fi
+
+echo "${CURRENT_VERSION} -> ${TARGET_VERSION}"
 
 bash -x ${BASEPATH}/upgrade-kubevirt.sh ${TARGET_VERSION}
 bash -x ${BASEPATH}/wait-kubevirt.sh

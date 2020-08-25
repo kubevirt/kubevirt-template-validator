113649d71a707b9676cad889fa6bdc23e51d20fe
Andrej Krejcir <akrejcir@redhat.com>
2020-08-25 11:18:21 +0200

    Set default values for CPU sockets, cores and threads
    If the fields are not defined in yaml, they have
    the default value 0. In this case kubevirt treats
    0 as 1, so template validator should too.
    
    Bug: https://bugzilla.redhat.com/show_bug.cgi?id=1870541
    
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- pkg/webhooks/validating/admission.go
+++ pkg/webhooks/validating/admission.go
@@ -36,6 +36,9 @@ func ValidateVMTemplate(rules []validation.Rule, newVM, oldVM *k6tv1.VirtualMach
 		log.Log.V(8).Infof("no admission rules for: %s", newVM.Name)
 		return causes
 	}
+
+	setDefaultValues(newVM)
+
 	buf := new(bytes.Buffer)
 	ev := validation.Evaluator{Sink: buf}
 	res := ev.Evaluate(rules, newVM)
@@ -45,4 +48,19 @@ func ValidateVMTemplate(rules []validation.Rule, newVM, oldVM *k6tv1.VirtualMach
 		return causes
 	}
 	return res.ToStatusCauses()
+}
+
+func setDefaultValues(vm *k6tv1.VirtualMachine) {
+	vmSpec := vm.Spec.Template.Spec
+	if vmSpec.Domain.CPU != nil {
+		if vmSpec.Domain.CPU.Sockets == 0 {
+			vmSpec.Domain.CPU.Sockets = 1
+		}
+		if vmSpec.Domain.CPU.Cores == 0 {
+			vmSpec.Domain.CPU.Cores = 1
+		}
+		if vmSpec.Domain.CPU.Threads == 0 {
+			vmSpec.Domain.CPU.Threads = 1
+		}
+	}
 }
